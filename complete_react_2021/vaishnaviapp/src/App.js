f241ec7d5f6664e199a494f6c262a387158a8b51
vaishnaviigitte <gittevaishnavijnec@gmail.com>
2024-04-05 22:04:42 +0530

    initial commit

---
+++ complete_react_2021/vaishnaviapp/src/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+
+import Sakshi from './component/Sakshi.js';
+// import UseState from './component/hooks/useState';
+// import  UseEffect  from './component/hooks/useEffect';
+// import useReducer  from './component/hooks/useReducer';
+// import Todo from './component/todoreact/todo';
+ 
+const App = () => {
+  return  
+ <Fragment>
+ hello
+ <Sakshi />
+</Fragment>
+  
+};
+
+export default App;
---
+++ complete_react_2021/vaishnaviapp/src/component/Sakshi.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sakshi = () => {
+  return (
+    <div>
+      <h1>sakshi</h1>
+    </div>
+  )
+}
+
+export default Sakshi
